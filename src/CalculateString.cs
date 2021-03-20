108a10efc6f7a3ef1b8a282623ef1e0baba53804
nonbinary-duck <webmaster@montugames.com>
2021-03-20 22:56:59 +0000

    Re-formatted CalculateString

--- src/CalculateString.cs
+++ src/CalculateString.cs
@@ -11,13 +11,13 @@ namespace rnd
         /// <summary>
         /// If the length is larger than or equal to this, the program will use all cpu threads to calculate the string
         /// </summary>
-        public static int multiThreadedLength = 15000;
+        public static long multiThreadedLength = 15000;
 
-        public static string s_MakeString(int length, char[] characterSet)
+        public static string MakeString(long length, char[] characterSet)
         {
-            if (length >= multiThreadedLength) return sm_MakeThreadedString(length, characterSet);
+            if (length >= multiThreadedLength) return MakeThreadedString(length, characterSet);
 
-            return sm_MakeString(length, characterSet);
+            return MakeNonThreadedString(length, characterSet);
         }
 
         /// <summary>
@@ -26,11 +26,11 @@ namespace rnd
         /// <param name="length">Length of string</param>
         /// <param name="characterSet">Characters to make the string from</param>
         /// <returns>Random string</returns>
-        protected static string sm_MakeString(int length, char[] characterSet)
+        protected static string MakeNonThreadedString(long length, char[] characterSet)
         {
             List<char> str = new List<char>();
 
-            sm_Transaction(ref str, characterSet, length, RandomNumberGenerator.GetInt32(int.MinValue, int.MaxValue));
+            Transaction(ref str, characterSet, length, RandomNumberGenerator.GetInt32(int.MinValue, int.MaxValue));
 
             return new string(str.ToArray());
         }
@@ -41,11 +41,11 @@ namespace rnd
         /// <param name="length">Length of string</param>
         /// <param name="characterSet">Characters to make the string from</param>
         /// <returns>Random string</returns>
-        protected static string sm_MakeThreadedString(int length, char[] characterSet)
+        protected static string MakeThreadedString(long length, char[] characterSet)
         {
             int threads = Environment.ProcessorCount;
-            int lenPerThread = length / threads;
-            int lastThreadLength = length - ((threads - 1) * lenPerThread);
+            long lenPerThread = length / threads;
+            long lastThreadLength = length - ((threads - 1) * lenPerThread);
 
             List<char>[] str = new List<char>[threads];
 
@@ -59,7 +59,7 @@ namespace rnd
 
                 tasks[i] = Task.Factory.StartNew(() =>
                 {
-                    sm_Transaction(ref str[p], characterSet, (p + 1 == threads) ? lastThreadLength : lenPerThread, RandomNumberGenerator.GetInt32(int.MinValue, int.MaxValue));
+                    Transaction(ref str[p], characterSet, (p + 1 == threads) ? lastThreadLength : lenPerThread, RandomNumberGenerator.GetInt32(int.MinValue, int.MaxValue));
                 });
             }
 
@@ -68,7 +68,7 @@ namespace rnd
             List<char> finalStr = new List<char>();
 
             // Consoledate information
-            for (int i = 0; i < str.Length; i++)
+            for (long i = 0; i < str.Length; i++)
             {
                 finalStr.AddRange(str[i]);
             }
@@ -77,17 +77,17 @@ namespace rnd
         }
 
         /// <summary>
-        /// Adds random info to a List<char> using a given seed for a System.Random
+        /// Adds random info to a List(char) using a given seed for a System.Random
         /// </summary>
-        /// <param name="str">The List<char> to add too</param>
+        /// <param name="str">The List(char) to add too</param>
         /// <param name="charSet">The character set to add to str</param>
         /// <param name="len">Number of items to add to str</param>
         /// <param name="seed">The seed to be given to the System.Random (please calc this using crypto)</param>
-        protected static void sm_Transaction(ref List<char> str, char[] charSet, int len, int seed)
+        protected static void Transaction(ref List<char> str, char[] charSet, long len, int seed)
         {
             Random rnd = new Random(seed);
 
-            for (int i = 0; i < len; i++)
+            for (long i = 0; i < len; i++)
             {
                 str.Add(charSet[rnd.Next(charSet.Length)]);
             }
